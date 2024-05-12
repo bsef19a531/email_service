d2d8ae873ec713af2975276fc10d94a4d9d5b6f4
Mohammad Abdullah <bsef19a531@pucit.edu.pk>
2024-05-12 20:00:23 +0500

    chore: Configure CORS for contact form submission

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const nodemailer = require('nodemailer');
 const sendEmail = require('./sendEmail');
+const cors = require('cors');
 
-// Configure Hostinger SMTP settings (replace with your credentials)
+// Configure CORS
+const corsMiddleware = cors({
+    methods: ['POST'],
+    origin: '*',
+});
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 587,  // Adjust if required based on Hostinger's configuration
@@ -13,6 +18,7 @@ const transporter = nodemailer.createTransport({
 });
 
 export default async function handler(req, res) {
+    await corsMiddleware(req, res);
     if (req.method === 'POST') {
         const { name, email, phone, service, message } = req.body;
         if (!name || !email || !message) {
@@ -27,6 +33,25 @@ export default async function handler(req, res) {
     } else {
         return res.status(405).json({ error: 'Method not allowed' });
     }
-
 }
+
+// Configure Hostinger SMTP settings (replace with your credentials)
+
+// export default async function handler(req, res) {
+//     if (req.method === 'POST') {
+//         const { name, email, phone, service, message } = req.body;
+//         if (!name || !email || !message) {
+//             return res.status(422).json({ error: 'All fields are required' });
+//         }
+//         try {
+//             await sendEmail(name, email, phone, service, message);
+//             return res.status(200).json({ message: 'Email sent successfully' });
+//         } catch (error) {
+//             return res.status(500).json({ error: 'An error occurred while sending the email' });
+//         }
+//     } else {
+//         return res.status(405).json({ error: 'Method not allowed' });
+//     }
+
+// }
 
