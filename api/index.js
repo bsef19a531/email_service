371480becf6ae0fbb1dcd18999716ba0d75543a4
Mohammad Abdullah <bsef19a531@pucit.edu.pk>
2024-05-24 03:25:21 +0500

    chore: Update email sending functionality with new password for nodemailer

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ const transporter = nodemailer.createTransport({
     secure: false,
     auth: {
         user: 'quatech.official@gmail.com',
-        pass: 'fzhcrzypiupozsip'
+        pass: 'pass'  // Replace with your Gmail app password
     }
 });
 
@@ -28,18 +28,34 @@ app.post('/', async (req, res) => {
         to: 'quatech.official@gmail.com',
         subject: `Contact Form Submission from ${name}`,
         text: `
-      Name: ${name}
-      Email: ${email}
-      Phone: ${phone}
-      Service: ${service}
-      Message: ${message}
-    `
+        Name: ${name}
+        Email: ${email}
+        Phone: ${phone}
+        Service: ${service}
+        Message: ${message}
+        `
+    };
+
+    const mailOptionsForClient = {
+        from: 'quatech.official@gmail.com',
+        to: email,
+        subject: `Thank you for contacting Quatechnologies, ${name}!`,
+        text: `
+        Hi ${name},
+        
+        Thank you for contacting Quatechnologies! We have received your message and will get back to you as soon as possible.
+        
+        Best regards,
+        Quatechnologies Team
+        `
     };
 
     try {
-        const info = await transporter.sendMail(mailOptions);
-        console.log(`Email sent successfully: ${info.response}`);
-        res.status(200).json({ message: 'Email sent successfully' });
+        const infoForCompany = await transporter.sendMail(mailOptions);
+        const infoForClient = await transporter.sendMail(mailOptionsForClient);
+        console.log(`Email sent successfully to company: ${infoForCompany.response}`);
+        console.log(`Email sent successfully to client: ${infoForClient.response}`);
+        res.status(200).json({ message: 'Emails sent successfully' });
     } catch (error) {
         console.error('Error sending email:', error);
         res.status(500).json({ error: `An error occurred while sending the email --> ${error}` });
